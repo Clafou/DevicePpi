04b70d3f7bdb323cdb4cb22bf10a8ee71867e463
Seb <clafou@gmail.com>
2018-09-28 11:22:19 +0100

    Initial Commit

---
+++ DevicePpiTests/DevicePpiTests.swift
@@ -0,0 +1,34 @@
+//
+//  DevicePpiTests.swift
+//  DevicePpiTests
+//
+//  Created by Sébastien Molines on 28/09/2018.
+//  Copyright © 2018 Sébastien Molines. All rights reserved.
+//
+
+import XCTest
+@testable import DevicePpi
+
+class DevicePpiTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
